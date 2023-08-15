916d352d9b7d36312e5aaf193d59be555bde90ea
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-08-15 19:52:33 +0530

    initial commit

---
+++ src/main/java/com/example/spotshare/SpotshareApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spotshare/SpotshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
